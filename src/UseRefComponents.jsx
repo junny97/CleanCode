da7f6a0df187ca3c4213a596825381b795c411eb
junny97 <pcohad12@gmail.com>
2024-02-21 15:56:32 +0900

    useRef로 리랜더링 방지하며 컴포넌트 마운트 정보를 확인하는

--- src/UseRefComponents.jsx
+++ src/UseRefComponents.jsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 //리랜더링 방지가 필요하다면 useState 보다 useRef 사용
 //컴포넌트의 전체적인 수명과 동일하게 지속된 정보를 일관적으로 제공해야 하는 경우
 export default function UseRefComponents() {
-  //useState를 사용해 컴포넌트 마운트 정보를 확인하는 방법
-  const [isMount, setIsMount] = useState(false);
+  //useRef로 리랜더링 방지하며 컴포넌트 마운트 정보를 확인하는
+  //useRef는 DOM 이벤트 조작만 다룬다는 생각 버리기!
+  const isMount = useRef(false);
 
   useEffect(() => {
-    if (!isMount) {
-      setIsMount(true);
-    }
-  }, [isMount]);
+    isMount.current = true;
+    return () => (isMount.current = false);
+  }, []);
 
-  return <div></div>;
+  return <div>{isMount && '컴포넌트 마운트 완료'}</div>;
 }
